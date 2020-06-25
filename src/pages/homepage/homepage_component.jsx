c88747936e6ba6519b32c1caea009c39aaa0cd5c
fazlay <rfazlay21@gmail.com>
2020-06-26 02:22:53 +0600

    first commit

---
+++ src/pages/homepage/homepage_component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directory.data'
+import './homepage_style.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
+
